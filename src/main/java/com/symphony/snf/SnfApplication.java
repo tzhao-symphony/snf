a3fae4c8efcf153b4f5c2398da9dc074e9cf04f1
Thomas ZHAO <78357406+tzhao-symphony@users.noreply.github.com>
2023-09-13 14:41:02 +0200

    BYC-900 Symphony News Feed initial spring boot setup

---
+++ src/main/java/com/symphony/snf/SnfApplication.java
@@ -0,0 +1,13 @@
+package com.symphony.snf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/symphony/snf/SnfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symphony.snf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
